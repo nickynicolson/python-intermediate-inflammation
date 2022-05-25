8505f74e99cbde23672bdac591a2fa8d3c394fe7
Nicky Nicolson <n.nicolson@kew.org>
2022-05-25 13:43:27 +0100

    Added edge case tests - -1, 0 and nan

--- inflammation/models.py
+++ inflammation/models.py
@@ -18,6 +18,22 @@ def load_csv(filename):
     return np.loadtxt(fname=filename, delimiter=',')
 
 
+def patient_normalise(data):
+    """
+        Normalise patient data from a 2D inflammation data array.
+
+        NaN values are ignored, and normalised to 0.
+
+        Negative values are rounded to 0.
+        """
+    max_data = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max_data[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
+
+
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
 
